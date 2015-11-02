a7c48cfb9b90a5dd5572a27f30098bd2b6173caf
Valeriy Yamkovyy <vydeveloping@gmail.com>
2015-11-02 19:39:49 +0200

    First Commit
    First Commit

---
+++ DiscountBank/DiscountBank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscountBank
+//
+//  Created by viera on 11/2/15.
+//  Copyright © 2015 viera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DiscountBank/DiscountBank/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DiscountBank
+//
+//  Created by viera on 11/2/15.
+//  Copyright © 2015 viera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
